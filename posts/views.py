6d2867abb54d847864d8fa3daea477210a3f7dac
Youngjun Kim <yomandawgshit@gmail.com>
2019-04-11 06:35:41 +0000

    views.py 업데이트 기능 추가

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Post
 from django.views.decorators.http import require_POST
 
 # Create your views here.
+def list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/list.html', {'posts': posts})
+
 def create(request):
     if request.method == "POST":
         # 작성된 post를 DB에 적용
@@ -15,30 +19,37 @@ def create(request):
         # post를 작성하는 form을 보여줌
         form = PostForm()
         return render(request, 'posts/create.html', {'form': form})
-
-def list(request):
-    posts = Post.objects.all()
-    return render(request, 'posts/list.html', {'posts': posts})
     
 def read(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     return render(request, 'posts/read.html', {'post': post})
 
 def update(request, post_id):
-    p = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     if request.method == "POST":
-        # 작성된 post를 DB에 적용
-        form = PostForm(request.POST, instance=p)
+        # 실제 DB에 수정 반영
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             form.save()
             return redirect('posts:list')
-    else: # GET
-        # post를 작성하는 form을 보여줌
-        form = PostForm(instance=p)
-        return render(request, 'posts/create.html', {'form': form})
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'posts/update.html', {'form': form})
+    
+    # p = Post.objects.get(id=post_id)
+    # if request.method == "POST":
+    #     # 작성된 post를 DB에 적용
+    #     form = PostForm(request.POST, instance=p)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('posts:list')
+    # else: # GET
+    #     # post를 작성하는 form을 보여줌
+    #     form = PostForm(instance=p)
+    #     return render(request, 'posts/create.html', {'form': form})
 
 # @require_POST
 def delete(request, post_id):
-    p = Post.objects.get(id=post_id)
-    p.delete()
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
     return redirect('posts:list')
