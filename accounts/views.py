dd57bf80544c591f9ae7a50b4ab846a062f1c3bc
Youngjun Kim <yomandawgshit@gmail.com>
2019-04-15 05:07:11 +0000

    people 프로파일 페이지 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login # django에서 가져와 씀
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
 
 
 def login(request):
@@ -33,10 +34,18 @@ def signup(request):
             user = form.save()
             # auth_login(request, form.get_user()) 로그인과 달리 form 형식 달라서 이거 안됨
             auth_login(request, user) # signup 후 바로 로그인
-            redirect('posts:list')
+            return redirect('posts:list')
     else:
         # GET: 유저 정보 입력
         form = UserCreationForm()
         
     return render(request, 'accounts/signup.html', {'form': form})
-        
+    
+
+def people(request, username):
+    # 사용자에 대한 정보
+    people = get_object_or_404(get_user_model(), username=username) # 모델과 keyword 인자로 가능
+    # 1. settings.AUTH_USER_MODEL - 이건 views를 쓰기 힘듦
+    # 2. get_user_model() 실제 user클래스를 바로 리턴시켜줌 - 되도록이면 이거 쓰자
+    # 3. User (django.contrib.auth.models에 들어가있는) 그냥 가져오기 - 안쓰는게 좋아
+    return render(request, 'accounts/people.html', {'people': people})
--- instagram/urls.py
+++ instagram/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
+    path('<str:username>/', accounts_views.people, name="people"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
